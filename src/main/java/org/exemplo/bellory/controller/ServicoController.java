a78d34c0f8652abcfb4ddf5b7153bbce840c8390
Guilherme Olivera <guilherme.oliveira@viannasempre.com.br>
2025-08-16 11:27:26 -0300

    teste

--- src/main/java/org/exemplo/bellory/controller/ServicoController.java
+++ src/main/java/org/exemplo/bellory/controller/ServicoController.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/servico")
+@CrossOrigin(origins = "*")
 public class ServicoController {
 
     private final ServicoService servicoService;
