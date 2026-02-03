0da261f9307f940610122e91bb609879bbbcd2b7
Guilherme Oliveira <guilherme.oliveira@viannasempre.com.br>
2026-02-03 13:15:48 -0300

    Ajustes

--- src/main/java/org/exemplo/bellory/BelloryApplication.java
+++ src/main/java/org/exemplo/bellory/BelloryApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class BelloryApplication {
 
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
         SpringApplication Bellory = new SpringApplication(BelloryApplication.class);
         Bellory.run(args);
     }
